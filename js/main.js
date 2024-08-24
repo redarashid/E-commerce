d7fb179e359ea3fd84ac781e2702254cfca6aa34
Radid reda <redarashid824@gmail.com>
2024-08-24 21:28:17 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+// Open_Cart and Close_Cart
+
+let cart = document.querySelector('.cart');
+
+function open_cart() {
+    cart.classList.add("active");
+}
+
+function close_cart() {
+    cart.classList.remove("active");
+}
